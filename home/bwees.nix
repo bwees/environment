58150c4d990ae10593c43693e5c99a7f4f74ec84
bwees <brandonwees@gmail.com>
2025-08-19 00:14:36 -0400

    fix: commit signing with git

--- home/bwees.nix
+++ home/bwees.nix
@@ -1,4 +1,9 @@
-{ config, inputs, pkgs, ... }:
+{
+  config,
+  inputs,
+  pkgs,
+  ...
+}:
 
 {
 
@@ -16,6 +21,16 @@
       };
       pull = {
         rebase = true;
+      };
+
+      # 1P Commit Signing
+      user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAuYih7LaCHvoa+3P6GjKtGpSWci/qX1OMjKRFk+TbIq";
+      gpg.format = "ssh";
+      gpg."ssh" = {
+        program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+      };
+      commit = {
+        gpgsign = true;
       };
     };
   };
