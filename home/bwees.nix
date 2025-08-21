4806174f5065a7f4592e179a5fb41597a9b860cf
bwees <brandonwees@gmail.com>
2025-08-21 00:29:19 -0400

    feat: configure ssh via home manager

--- home/bwees.nix
+++ home/bwees.nix
@@ -9,6 +9,15 @@
 
   home.stateVersion = "25.11";
 
+  programs.ssh = {
+    enable = true;
+    extraConfig = ''
+      Host *
+        IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+        SetEnv TERM=xterm-256color
+    '';
+  };
+
   programs.git = {
     enable = true;
     userEmail = "brandonwees@gmail.com";
